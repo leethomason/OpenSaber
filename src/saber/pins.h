519d0b77041a0b80a14c625d32e94962d71d387d
Lee Thomason <leethomason@gmail.com>
2016-03-27 20:16:51 -0700

    update pins

--- src/saber/pins.h
+++ src/saber/pins.h
@@ -32,7 +32,7 @@ static const int EEPROM_SIZE = 512;
 //#define SABER_ACCELEROMETER
 //#define SABER_SOUND_ON
 //#define SABER_SOUND_SHUTDOWN
-//#define SABER_VOLTMETER
+#define SABER_VOLTMETER
 //#define SABER_LOGGING
 //#define SABER_DISPLAY
 //#define SABER_CRYSTAL
