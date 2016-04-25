6536f8076e55448a9d5f19807ef19b8af563b73b
Lee Thomason <leethomason@gmail.com>
2016-04-24 22:57:01 -0700

    basic dotstar test

--- src/dotstar/dotstar.ino
+++ src/dotstar/dotstar.ino
@@ -1,26 +1,21 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 1
+#define NUM_LEDS 4
 CRGB leds[NUM_LEDS];
 
 void setup() {
   delay(200);
-  //FastLED.setBrightness(4);
+  FastLED.setBrightness(20);
   FastLED.addLeds<APA102, BGR>(leds, NUM_LEDS);
   pinMode(7, OUTPUT);
   digitalWrite(7, HIGH);  // enable access to LEDs
+
+  leds[0] = CRGB::Red;
+  leds[1] = CRGB::Green;
+  leds[2] = CRGB::Blue;
+  leds[3] = CRGB::Yellow;
+  FastLED.show(); 
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
-  FastLED.show(); 
-  delay(400);
-  
-  leds[0] = CRGB::Green;
-  FastLED.show(); 
-  delay(400);
-
-  leds[0] = CRGB::Blue;
-  FastLED.show(); 
-  delay(400);
 }
