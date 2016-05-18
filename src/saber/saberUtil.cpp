f51e85659e8e4d594adfa4626bd65faaed1606bc
Lee Thomason <leethomason@gmail.com>
2016-05-17 22:42:55 -0700

    formatting changes

--- src/saber/saberUtil.cpp
+++ src/saber/saberUtil.cpp
@@ -6,50 +6,60 @@
 
 void BladeState::change(uint8_t state)
 {
-	ASSERT(state >= BLADE_OFF && state <= BLADE_RETRACT);
-  	m_currentState   = state;
-  	m_startTime = millis();
+    ASSERT(state >= BLADE_OFF && state <= BLADE_RETRACT);
+    m_currentState   = state;
+    m_startTime = millis();
 
-  	switch(m_currentState) {
-  		case BLADE_OFF:		Log.event("[BLADE_OFF]");		break;
-  		case BLADE_IGNITE:	Log.event("[BLADE_IGNITE]");	break;
-  		case BLADE_ON:		Log.event("[BLADE_ON]");		break;
-  		case BLADE_FLASH:	Log.event("[BLADE_FLASH]");		break;
-  		case BLADE_RETRACT:	Log.event("[BLADE_RETRACT]");	break;
-  		default:
-  			ASSERT(false);
-  			break;
-  	}
+    switch(m_currentState) {
+    case BLADE_OFF:
+        Log.event("[BLADE_OFF]");
+        break;
+    case BLADE_IGNITE:
+        Log.event("[BLADE_IGNITE]");
+        break;
+    case BLADE_ON:
+        Log.event("[BLADE_ON]");
+        break;
+    case BLADE_FLASH:
+        Log.event("[BLADE_FLASH]");
+        break;
+    case BLADE_RETRACT:
+        Log.event("[BLADE_RETRACT]");
+        break;
+    default:
+        ASSERT(false);
+        break;
+    }
 }
 
-bool BladeState::bladeOn() const 
+bool BladeState::bladeOn() const
 {
-	ASSERT(m_currentState >= BLADE_OFF && m_currentState <= BLADE_RETRACT);
-	return m_currentState >= BLADE_ON && m_currentState < BLADE_RETRACT;
+    ASSERT(m_currentState >= BLADE_OFF && m_currentState <= BLADE_RETRACT);
+    return m_currentState >= BLADE_ON && m_currentState < BLADE_RETRACT;
 }
 
-void ButtonMode::nextMode() 
+void ButtonMode::nextMode()
 {
-	m_mode = (m_mode + 1) % NUM_BUTTON_MODES;
+    m_mode = (m_mode + 1) % NUM_BUTTON_MODES;
 }
 
 AveragePower::AveragePower()
 {
-  for(int i=0; i<NUM_SAMPLES; ++i) {
-    m_sample[i] = NOMINAL_VOLTAGE;
-  }
-  m_power = NOMINAL_VOLTAGE;
+    for(int i=0; i<NUM_SAMPLES; ++i) {
+        m_sample[i] = NOMINAL_VOLTAGE;
+    }
+    m_power = NOMINAL_VOLTAGE;
 }
 
 void AveragePower::push(uint32_t milliVolts)
 {
-  uint32_t total = milliVolts;
-  for(int i=0; i<NUM_SAMPLES-1; ++i) {
-    m_sample[i] = m_sample[i+1];
-    total += m_sample[i];    
-  }
-  m_sample[NUM_SAMPLES-1] = milliVolts;
-  m_power = total / NUM_SAMPLES;
+    uint32_t total = milliVolts;
+    for(int i=0; i<NUM_SAMPLES-1; ++i) {
+        m_sample[i] = m_sample[i+1];
+        total += m_sample[i];
+    }
+    m_sample[NUM_SAMPLES-1] = milliVolts;
+    m_power = total / NUM_SAMPLES;
 }
 
 
--- src/saber/saberUtil.h
+++ src/saber/saberUtil.h
@@ -3,68 +3,78 @@
 
 #include <stdint.h>
 
-enum {  
-	BLADE_OFF,
+enum {
+    BLADE_OFF,
     BLADE_IGNITE,
     BLADE_ON,
     BLADE_FLASH,
     BLADE_RETRACT
- };
+};
 
 class BladeState
 {
 public:
-	BladeState() {}
+    BladeState() {}
 
-     void change(uint8_t newState);
-     const int state() const { return m_currentState; }
+    void change(uint8_t newState);
+    const int state() const {
+        return m_currentState;
+    }
 
-     // Any of the blade-on states, not just the BLADE_ON idles state.
-     bool bladeOn() const;
-     bool bladeOff() const { return m_currentState == BLADE_OFF; }
+    // Any of the blade-on states, not just the BLADE_ON idles state.
+    bool bladeOn() const;
+    bool bladeOff() const {
+        return m_currentState == BLADE_OFF;
+    }
 
-     const uint32_t startTime() const { return m_startTime; }
+    const uint32_t startTime() const {
+        return m_startTime;
+    }
 
 private:
-	uint8_t  m_currentState = BLADE_OFF;
-	uint32_t m_startTime = 0;
+    uint8_t  m_currentState = BLADE_OFF;
+    uint32_t m_startTime = 0;
 };
 
 enum {
-	BUTTON_MODE_NORMAL,
-	BUTTON_MODE_PALETTE,
-	BUTTON_MODE_VOLUME,
-	NUM_BUTTON_MODES
+    BUTTON_MODE_NORMAL,
+    BUTTON_MODE_PALETTE,
+    BUTTON_MODE_VOLUME,
+    NUM_BUTTON_MODES
 };
 
 class ButtonMode
 {
 public:
-	ButtonMode() {}
+    ButtonMode() {}
 
-	void nextMode();
-	const int mode() const { return m_mode; }
+    void nextMode();
+    const int mode() const {
+        return m_mode;
+    }
 
 private:
-	uint8_t m_mode = BUTTON_MODE_NORMAL;
+    uint8_t m_mode = BUTTON_MODE_NORMAL;
 };
 
 /**
- * Power changes over time, and higher 
+ * Power changes over time, and higher
  * draw changes the power. A small class
  * to average out power changes.
  */
 class AveragePower
 {
 public:
-	AveragePower();
-	void push(uint32_t milliVolts);
-	uint32_t power() { return m_power; }
-	enum { NUM_SAMPLES = 8};
+    AveragePower();
+    void push(uint32_t milliVolts);
+    uint32_t power() {
+        return m_power;
+    }
+    enum { NUM_SAMPLES = 8};
 
 private:
-	uint32_t m_power;
-	uint32_t m_sample[NUM_SAMPLES];
+    uint32_t m_power;
+    uint32_t m_sample[NUM_SAMPLES];
 };
 
 
