8647507733a72463d02f2ef3f51d59b68e4d9989
Lee Thomason <leethomason@gmail.com>
2017-05-15 17:42:59 -0700

    convert to perceived color. fix a bunch of formatting.

--- src/saber/blade.cpp
+++ src/saber/blade.cpp
@@ -29,76 +29,76 @@ Blade* Blade::instance = 0;
 static const int32_t LED_RANGE = 255;
 
 Blade::Blade() {
-  for (int i = 0; i < NCHANNELS; ++i) {
+    for (int i = 0; i < NCHANNELS; ++i) {
 //    digitalWrite(pinRGB[i], LOW);
-    pinMode(pinRGB[i], OUTPUT);
-    digitalWrite(pinRGB[i], LOW);
-    pwm[i] = 0;
-  }
+        pinMode(pinRGB[i], OUTPUT);
+        digitalWrite(pinRGB[i], LOW);
+        pwm[i] = 0;
+    }
 
-  vbat   = NOMINAL_VOLTAGE;
-  for (int i = 0; i < NCHANNELS; ++i) {
-    f1000[i] = 1000;
-    color[i] = 0;
-  }
-  instance = this;
+    vbat   = NOMINAL_VOLTAGE;
+    for (int i = 0; i < NCHANNELS; ++i) {
+        f1000[i] = 1000;
+        color[i] = 0;
+    }
+    instance = this;
 }
 
 
 uint8_t Blade::lerpU8(uint8_t a, uint8_t b, uint8_t t) const {
-  int32_t r = int32_t(a) + (int32_t(b) - int32_t(a)) * int32_t(t) / 255;
-  return uint8_t(constrain(r, 0, 255));
+    int32_t r = int32_t(a) + (int32_t(b) - int32_t(a)) * int32_t(t) / 255;
+    return uint8_t(constrain(r, 0, 255));
 }
 
 
 void Blade::setRGB(const RGB& input)
 {
-  static const int32_t DIV = int32_t(255) * int32_t(1000);
-  for (int i = 0; i < NCHANNELS; ++i ) {
-    color[i] = input[i];
-    int32_t v = LED_RANGE * f1000[i] * int32_t(input[i]) / DIV;
-    pwm[i] = constrain(v, 0, 255);  // just in case...
+    static const int32_t DIV = int32_t(255) * int32_t(1000);
+    for (int i = 0; i < NCHANNELS; ++i ) {
+        color[i] = input[i];
+        int32_t v = LED_RANGE * f1000[i] * int32_t(input[i]) / DIV;
+        pwm[i] = constrain(v, 0, 255);  // just in case...
 
 #if SERIAL_DEBUG == 1
-    /*
-      Serial.print("setRGB:");
-      Serial.print(i);
-      Serial.print(" input:" ); Serial.print(input[i]);
-      Serial.print(" f:"); Serial.print(f1000[i]);
-      Serial.print(" v:"); Serial.print(v);
-      Serial.print(" div:"); Serial.print(DIV);
-      Serial.print(" output:"); Serial.println(prime);
-    */
+        /*
+          Serial.print("setRGB:");
+          Serial.print(i);
+          Serial.print(" input:" ); Serial.print(input[i]);
+          Serial.print(" f:"); Serial.print(f1000[i]);
+          Serial.print(" v:"); Serial.print(v);
+          Serial.print(" div:"); Serial.print(DIV);
+          Serial.print(" output:"); Serial.println(prime);
+        */
 #endif
-    analogWrite(pinRGB[i], pwm[i]);
-  }
+        analogWrite(pinRGB[i], pwm[i]);
+    }
 }
 
 
 bool Blade::setInterp(uint32_t delta, uint32_t effectTime, const RGB& startColor, const RGB& endColor)
 {
-  if (delta >= effectTime) {
-    setRGB(endColor);
-    return true;
-  }
-  else {
-    uint8_t t = uint8_t(255 * delta / effectTime);
-    RGB color;
-    for (int i = 0; i < NCHANNELS; ++i) {
-      color[i] = lerpU8(startColor[i], endColor[i], t);
+    if (delta >= effectTime) {
+        setRGB(endColor);
+        return true;
     }
-    setRGB(color);
-  }
-  return false;
+    else {
+        uint8_t t = uint8_t(255 * delta / effectTime);
+        RGB color;
+        for (int i = 0; i < NCHANNELS; ++i) {
+            color[i] = lerpU8(startColor[i], endColor[i], t);
+        }
+        setRGB(color);
+    }
+    return false;
 }
 
 
 void Blade::setVoltage(int milliVolts) {
-  // If a driver is in use, just use straight PWM, and 
-  // leave f1000 = 1000. If a resistor is in use,
-  // account for the voltage being above design voltage.
+    // If a driver is in use, just use straight PWM, and
+    // leave f1000 = 1000. If a resistor is in use,
+    // account for the voltage being above design voltage.
 
-  #if (LED_TOPOLOGY == LED_TOPOLOGY_RESISTOR) 
+#if (LED_TOPOLOGY == LED_TOPOLOGY_RESISTOR)
     static const int32_t f = 256;   // 256 is "use new value"
     static const int32_t m = 256 - f;
     int32_t vF[NCHANNELS]   = { RED_VF, GREEN_VF, BLUE_VF };
@@ -108,40 +108,58 @@ void Blade::setVoltage(int milliVolts) {
     vbat = (int32_t(milliVolts) * f + vbat * m) / int32_t(256);
 
     for (int i = 0; i < NCHANNELS; ++i) {
-      f1000[i] = amps[i] * res[i] / (vbat - vF[i]);
-      if (f1000[i] > 1000) {
-        f1000[i] = 1000;
-      }
+        f1000[i] = amps[i] * res[i] / (vbat - vF[i]);
+        if (f1000[i] > 1000) {
+            f1000[i] = 1000;
+        }
     }
-  #elif (LED_TOPOLOGY == LED_TOPOLOGY_DRIVER)
+#elif (LED_TOPOLOGY == LED_TOPOLOGY_DRIVER)
     vbat = int32_t(milliVolts);  // used for reporting on the command line, but f1000 isn't change,
-                                // so therefore the blade power doesn't change.
-  #endif
-  setRGB(color);
+    // so therefore the blade power doesn't change.
+#endif
+    setRGB(color);
 }
 
 
 int32_t Blade::power() const {
-  int32_t amps = 0;
-  #if (LED_TOPOLOGY == LED_TOPOLOGY_RESISTOR) 
+    int32_t amps = 0;
+#if (LED_TOPOLOGY == LED_TOPOLOGY_RESISTOR)
     static const int32_t cV[NCHANNELS] = { RED_VF, GREEN_VF, BLUE_VF };
     static const int32_t cR[NCHANNELS] = { RED_R, GREEN_R, BLUE_R };
 
     for (int i = 0; i < NCHANNELS; ++i) {
-      int32_t cI = int32_t(1000) * (NOMINAL_VOLTAGE - cV[i]) / cR[i];
+        int32_t cI = int32_t(1000) * (NOMINAL_VOLTAGE - cV[i]) / cR[i];
 
-      int32_t aChannel = int32_t(LED_RANGE) * cI * int32_t(color[i]) / int32_t(65025);
-      amps += aChannel;
+        int32_t aChannel = int32_t(LED_RANGE) * cI * int32_t(color[i]) / int32_t(65025);
+        amps += aChannel;
     }
-  #elif (LED_TOPOLOGY == LED_TOPOLOGY_DRIVER)
+#elif (LED_TOPOLOGY == LED_TOPOLOGY_DRIVER)
     static const int32_t cA[NCHANNELS] = { RED_I, GREEN_I, BLUE_I };
 
     for (int i = 0; i < NCHANNELS; ++i) {
-      int32_t aChannel = cA[i] * int32_t(color[i]) / int32_t(255);
-      amps += aChannel;
-    }    
-  #else
-    #error Topology not defined.
-  #endif
-  return amps;
+        int32_t aChannel = cA[i] * int32_t(color[i]) / int32_t(255);
+        amps += aChannel;
+    }
+#else
+#error Topology not defined.
+#endif
+    return amps;
+}
+
+
+/*static*/ RGB Blade::convertRawToPerceived(const RGB& raw)
+{
+    #ifdef LED_TYPE 
+        RGB rgb;
+        #if (LED_TYPE == BBG)
+            rgb.r = 0;
+            rgb.g = (raw.r + raw.g) / 2;
+            rgb.b = raw.b;
+        #else
+            #error LED_TYPE not defined.
+        #endif
+        return rgb;
+    #else
+        return raw;
+    #endif
 }
--- src/saber/saberdb.cpp
+++ src/saber/saberdb.cpp
@@ -1,22 +1,22 @@
 /*
 Copyright (c) 2016 Lee Thomason, Grinning Lizard Software
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of 
-this software and associated documentation files (the "Software"), to deal in 
-the Software without restriction, including without limitation the rights to 
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is furnished to do 
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
 so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all 
+The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
@@ -28,154 +28,168 @@ SOFTWARE.
 
 
 SaberDB::SaberDB() {
-  STATIC_ASSERT(BASE_ADDR + NUM_PALETTES * sizeof(Palette) + sizeof(DataHeader) < EEPROM_SIZE);
+    STATIC_ASSERT(BASE_ADDR + NUM_PALETTES * sizeof(Palette) + sizeof(DataHeader) < EEPROM_SIZE);
 }
 
-bool SaberDB::writeDefaults() 
+bool SaberDB::writeDefaults()
 {
-  static Palette defPalette[NUM_PALETTES] = {
-    { 0x00ff00,  0x00ffa0,    0 },    // green
-    { 0x0000ff,  0x00c8ff,    0 },    // blue
-    { 0x00ffff,  0x00a0ff,    0 },    // cyan
-    { 0xff0000,  0xa08000,    0 },    // red
-    { 0xff4000,  0x80ff00,    0 },
-    { 0x0044ff,  0x00ccff,    0 },
-    { 0x9000ff,  0x9064ff,    0 },
-    { 0x00ff44,  0x00ffaa,    0 } 
-  };
+    static Palette defPalette[NUM_PALETTES] = {
+        { 0x00ff00,  0x00ffa0,    0 },    // green
+        { 0x0000ff,  0x00c8ff,    0 },    // blue
+        { 0x00ffff,  0x00a0ff,    0 },    // cyan
+        { 0xff0000,  0xa08000,    0 },    // red
+        { 0xff4000,  0x80ff00,    0 },
+        { 0x0044ff,  0x00ccff,    0 },
+        { 0x9000ff,  0x9064ff,    0 },
+        { 0x00ff44,  0x00ffaa,    0 }
+    };
 
-  static const char* defNames[NUM_PALETTES] = {
-    "BESPIN2", 
-    "OBIWAN", 
-    "FATES", 
-    "VADER",
-    "ROGUE", 
-    "GRAFLEX", 
-    "JAINA", 
-    "ROGUE"
-  };
+    static const char* defNames[NUM_PALETTES] = {
+        "BESPIN2",
+        "OBIWAN",
+        "FATES",
+        "VADER",
+        "ROGUE",
+        "GRAFLEX",
+        "JAINA",
+        "ROGUE"
+    };
 
-  Log.p("Writing EEPROM default.").eol();
+    Log.p("Writing EEPROM default.").eol();
 
-  DataHeader dataHeaderDefault;
-  EEPROM.put(headerAddr(), dataHeaderDefault);
+    DataHeader dataHeaderDefault;
+    EEPROM.put(headerAddr(), dataHeaderDefault);
 
-  for (int i = 0; i < NUM_PALETTES; ++i) {
-    Palette p = defPalette[i];
-    #if SABER_SOUND_ON == SABER_SOUND_FLASH
-    p.soundFont = "<flash>";
-    #else
-    p.soundFont = defNames[i];
-    #endif
-    EEPROM.put(paletteAddr(i), p);
-  }
-  readData();
-  Log.p("EEPROM default complete.").eol();
-  return true;
+    for (int i = 0; i < NUM_PALETTES; ++i) {
+        Palette p = defPalette[i];
+#if SABER_SOUND_ON == SABER_SOUND_FLASH
+        p.soundFont = "<flash>";
+#else
+        p.soundFont = defNames[i];
+#endif
+        EEPROM.put(paletteAddr(i), p);
+    }
+    readData();
+    Log.p("EEPROM default complete.").eol();
+    return true;
 }
 
 
 bool SaberDB::readData() {
 
-  EEPROM.get(headerAddr(), dataHeader);
-  EEPROM.get(paletteAddr(dataHeader.currentPalette), palette);
-  /*
-  Serial.print(F("Data header. currentPalette=")); Serial.print(dataHeader.currentPalette);
-  Serial.print(F(" soundOn=")); Serial.println(dataHeader.soundOn);
-  Serial.print(F("RGB ")); Serial.print(palette.bladeColor[0]); Serial.print(" "); Serial.print(palette.bladeColor[1]); Serial.print(" "); Serial.println(palette.bladeColor[2]);
-  */
-  setupInit();
-  return true;
+    EEPROM.get(headerAddr(), dataHeader);
+    EEPROM.get(paletteAddr(dataHeader.currentPalette), palette);
+    /*
+    Serial.print(F("Data header. currentPalette=")); Serial.print(dataHeader.currentPalette);
+    Serial.print(F(" soundOn=")); Serial.println(dataHeader.soundOn);
+    Serial.print(F("RGB ")); Serial.print(palette.bladeColor[0]); Serial.print(" "); Serial.print(palette.bladeColor[1]); Serial.print(" "); Serial.println(palette.bladeColor[2]);
+    */
+    setupInit();
+    return true;
 }
 
 
 void SaberDB::nextPalette() {
-  setPalette(dataHeader.currentPalette + 1);
+    setPalette(dataHeader.currentPalette + 1);
 }
 
-void SaberDB::setPalette(int n) 
+void SaberDB::setPalette(int n)
 {
 #ifdef SABER_SOUND_ON
-  // The SD card memory streaming and the use of the SD
-  // file system lead to crashes. (Grr.) Stop the sound 
-  // here to see if it cleans up the problem.
-  SFX::instance()->stopSound();
+    // The SD card memory streaming and the use of the SD
+    // file system lead to crashes. (Grr.) Stop the sound
+    // here to see if it cleans up the problem.
+    SFX::instance()->stopSound();
 #endif
 
-  dataHeader.currentPalette = abs(n) % NUM_PALETTES;
-  Log.event("[PALETTE]", dataHeader.currentPalette);
-  //Log.p("Switch Palette to: ").p(dataHeader.currentPalette).eol();
+    dataHeader.currentPalette = abs(n) % NUM_PALETTES;
+    Log.event("[PALETTE]", dataHeader.currentPalette);
+    //Log.p("Switch Palette to: ").p(dataHeader.currentPalette).eol();
 
-  EEPROM.put(headerAddr(), dataHeader);
-  EEPROM.get(paletteAddr(dataHeader.currentPalette), palette);
+    EEPROM.put(headerAddr(), dataHeader);
+    EEPROM.get(paletteAddr(dataHeader.currentPalette), palette);
 }
 
-void SaberDB::setupInit() 
+void SaberDB::setupInit()
 {
-  dataHeader.nSetup = dataHeader.nSetup + 1;
-  EEPROM.put(headerAddr(), dataHeader);
+    dataHeader.nSetup = dataHeader.nSetup + 1;
+    EEPROM.put(headerAddr(), dataHeader);
 }
 
 bool SaberDB::setSoundOn(bool on) {
-  dataHeader.soundOn = on;
-  Log.p("setSoundOn sound: ").p(dataHeader.soundOn).eol();
+    dataHeader.soundOn = on;
+    Log.p("setSoundOn sound: ").p(dataHeader.soundOn).eol();
 
-  EEPROM.put(headerAddr(), dataHeader);
-  return true;
+    EEPROM.put(headerAddr(), dataHeader);
+    return true;
 }
 
 void SaberDB::setVolume(int v) {
-  v = constrain(v, 0, 204);
-  dataHeader.volume = v;
-  EEPROM.put(headerAddr(), dataHeader);
+    v = constrain(v, 0, 204);
+    dataHeader.volume = v;
+    EEPROM.put(headerAddr(), dataHeader);
 }
 
-void SaberDB::setVolume4(int v) 
+void SaberDB::setVolume4(int v)
 {
-  switch (v) {
-    case 0: setSoundOn(false); break;
-    case 1: setVolume(VOLUME_1); setSoundOn(true); break;
-    case 2: setVolume(VOLUME_2); setSoundOn(true); break;
-    case 3: setVolume(VOLUME_3); setSoundOn(true); break;
-    case 4: setVolume(VOLUME_4); setSoundOn(true); break;
-  }
+    switch (v) {
+    case 0:
+        setSoundOn(false);
+        break;
+    case 1:
+        setVolume(VOLUME_1);
+        setSoundOn(true);
+        break;
+    case 2:
+        setVolume(VOLUME_2);
+        setSoundOn(true);
+        break;
+    case 3:
+        setVolume(VOLUME_3);
+        setSoundOn(true);
+        break;
+    case 4:
+        setVolume(VOLUME_4);
+        setSoundOn(true);
+        break;
+    }
 }
 
 uint8_t SaberDB::volume4() const
 {
-  if (!soundOn() || dataHeader.volume == 0) {
-    return 0;
-  }
-  if (dataHeader.volume == VOLUME_4) return 4;
-  if (dataHeader.volume >= VOLUME_3) return 3;
-  if (dataHeader.volume >= VOLUME_2) return 2;
-  return 1;
+    if (!soundOn() || dataHeader.volume == 0) {
+        return 0;
+    }
+    if (dataHeader.volume == VOLUME_4) return 4;
+    if (dataHeader.volume >= VOLUME_3) return 3;
+    if (dataHeader.volume >= VOLUME_2) return 2;
+    return 1;
 }
 
 void SaberDB::setMotion(float v) {
-  v = constrain(v, 1.1, 10.0);
-  dataHeader.motion = v;
-  EEPROM.put(headerAddr(), dataHeader);
+    v = constrain(v, 1.1, 10.0);
+    dataHeader.motion = v;
+    EEPROM.put(headerAddr(), dataHeader);
 }
 
 void SaberDB::setImpact(float v) {
-  v = constrain(v, 1.1, 10.0);
-  dataHeader.impact = v;
-  EEPROM.put(headerAddr(), dataHeader);
+    v = constrain(v, 1.1, 10.0);
+    dataHeader.impact = v;
+    EEPROM.put(headerAddr(), dataHeader);
 }
 
 void SaberDB::setBladeColor(const RGB& color) {
-  palette.bladeColor = color;
-  EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
+    palette.bladeColor = color;
+    EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
 }
 
 void SaberDB::setImpactColor(const RGB& color) {
-  palette.impactColor = color;
-  EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
+    palette.impactColor = color;
+    EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
 }
 
 void SaberDB::setSoundFont(const char* v) {
-  palette.soundFont = v;
-  EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
+    palette.soundFont = v;
+    EEPROM.put(paletteAddr(dataHeader.currentPalette), palette);
 }
 
