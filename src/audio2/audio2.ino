9865ef2ef331fe79996920b36546d36a10a37c9a
Lee Thomason <leethomason@gmail.com>
2018-06-24 15:02:51 -0700

    clear 440 tone.

--- src/audio2/audio2.ino
+++ src/audio2/audio2.ino
@@ -3,6 +3,11 @@
 uint32_t startTime = 0;
 uint32_t lastTime = 0;
 
+static const uint32_t SECOND =  1000000UL;
+static const uint32_t CYCLE = SECOND / 440UL;
+const float VOLUME = 0.4f;
+uint16_t buffer[CYCLE];
+
 void setup() {
     /*
     Serial.begin(19200);
@@ -19,9 +24,17 @@ void setup() {
     analogWrite(A0, 1023);
     pinMode(7, OUTPUT);
     digitalWrite(7, HIGH);
+
+    for(int x=0; x<CYCLE; ++x) {
+        float d = 6.283f * float(x) / float(CYCLE);
+        float v = sin(d) * VOLUME + 1.0f;
+
+        buffer[x] = uint32_t(512.0 * v);
+    }
 }
 
 void loop() {
+    /*
     uint32_t t = millis();
     if (lastTime != t) {
         if (t&1)
@@ -29,31 +42,11 @@ void loop() {
         else
             analogWrite(A0, 490);
         lastTime = t;
-    }
-    /*
+    }*/
+   
     uint32_t time = micros();
     uint32_t delta = time - startTime;
-
-    //Serial.print("delta="); Serial.println(delta);
-
-    // 440Hz
-    static const uint32_t SECOND =  1000000UL;
-    static const uint32_t CYCLE = SECOND / 440UL;
-
     uint32_t subCycle = delta % CYCLE;
-    //Serial.print("subCycle="); Serial.println(subCycle);
-
-    float d = 6.283f * float(subCycle) / float(CYCLE);
-    float v = sin(d) + 1.0f;
-    //Serial.print("v="); Serial.println(v);
-    const float VOL = 0.5;
-    float vPrime = v * VOL;
-
-    int32_t v16 = int32_t(1024.0 * vPrime);
-    //Serial.print("v16="); Serial.println(v16);
-
-    analogWrite(A0, v16);
-    */
-    //while(true) {}
+    analogWrite(A0, buffer[subCycle]);
 }
 
