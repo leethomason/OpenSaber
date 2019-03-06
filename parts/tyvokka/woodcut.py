9293642705c4f5e20fd9b7393a759d2d01cd5776
Lee Thomason <leethomason@gmail.com>
2019-03-05 19:00:11 -0800

    big cleanup of gcode api

--- parts/tyvokka/woodcut.py
+++ parts/tyvokka/woodcut.py
@@ -6,122 +6,68 @@ from plane import plane
 from covertec import hill
 from hole import hole
 
+# Top of stock.
 H_WOOD = 8.0
 H_STOCK = 12.0
-Z_PAD = 0.8
-TOOL_R = 3.175 / 2
-SMOOTH_PATHS = False
-MAT = 'np883-hardwood-3.175'
+X_CAPSULE = 42.0
+Y_CAPSULE = 16.2
+Z_PAD = 0.5
+TOOL = 3.175
+DZ_CENTER = (X_CAPSULE - Y_CAPSULE) / 2
+MAT = 'np883-hardwood-{0}'.format(TOOL)
+D_OUTER = 32.0  # FIXME
 
-Z_STOCK = H_STOCK - H_WOOD
-DELTA = 3
-SMALL = 0.5
 
-X_CAPSULE_NOM = 42.0
-D_CAPSULE = 16.2
-DZ_CENTER = (X_CAPSULE_NOM - D_CAPSULE) / 2
+center = Rectangle(0, -Y_CAPSULE/2, X_CAPSULE, Y_CAPSULE/2)
+bounds = Bounds(TOOL, center)
+bottom = -H_WOOD - Z_PAD
+top = -(H_STOCK - H_WOOD)
 
-R = D_CAPSULE / 2
-D_OUTER = 37.9
-
-TRAVEL = H_STOCK + 3
-
-# origin at left end of piece.
 mat = init_material(MAT)
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None, print_lines=False)
 nomad_header(g, mat, CNC_TRAVEL_Z)
 
 g.absolute()
-g.move(z=TRAVEL)
-
-g.spindle('CW', mat['spindle_speed'])
-g.feed(mat['travel_feed'])
-
-# flatten and take off the top of stock
-if Z_STOCK > 0:
-    g.move(x=-DELTA, y=-D_CAPSULE/2-DELTA)
-    g.move(z=Z_STOCK)
-    g.feed(mat['feed_rate'])
-    plane(g, mat, H_WOOD - H_STOCK, X_CAPSULE_NOM + DELTA * 2, D_CAPSULE + DELTA * 2)
-
-# cut the curve
-g.move(z=CNC_TRAVEL_Z)
-g.spindle()
-g.move(x=-DELTA, y=-SMALL)
-g.move(z=Z_STOCK)
 g.move(z=0)
 
-hill(g, mat, D_OUTER, X_CAPSULE_NOM + DELTA*2, D_CAPSULE+SMALL*2)
-g.move(z=CNC_TRAVEL_Z)
+if (H_STOCK < H_WOOD):
+    raise RuntimeError("Stock can be the same as the wood, but not lower.")
 
-# insets
-g.feed(mat['travel_feed'])
+if H_STOCK > H_WOOD:
+    plane(g, mat, top, bounds.outer.x0, bounds.outer.y0, bounds.outer.x1, bounds.outer.y1)
+
+g.move(z=top)
+
+# cut the curve
+hill(g, mat, D_OUTER, bounds.center.dx, bounds.center.dy)
 
 #center bolt
 HEAD = 7.0
 HEAD_H = 4.5
 BOLT = 4.3
-g.move(x=X_CAPSULE_NOM/2, y=0)
 
-g.move(z=CNC_TRAVEL_Z)
-
-g.feed(mat['feed_rate'])
-hole(g, mat, -HEAD_H, HEAD/2)
-hole(g, mat, -H_WOOD - Z_PAD, BOLT/2)
+travel(g, mat, x=bounds.cx)
+hole(g, mat, -HEAD_H, d=HEAD)
+hole(g, mat, bottom, d=BOLT)
 
 # power
 POWER_D = 8.0
 POWER_OUTER_D = 11.0
 POWER_OUTER_DEPTH = -4.0
-POWER_X = X_CAPSULE_NOM/2 - DZ_CENTER
 
-g.move(z=CNC_TRAVEL_Z)
-g.move(x=POWER_X, y=0)
-hole(g, mat, -H_WOOD - Z_PAD, POWER_D/2)
-
-if SMOOTH_PATHS:
-    # rounded inset
-    hole(g, mat, POWER_OUTER_DEPTH, POWER_OUTER_D/2 - TOOL_R)
-else:
-    hole(g, mat, POWER_OUTER_DEPTH, POWER_OUTER_D/2)
+travel(g, mat, x=bounds.cx - DZ_CENTER)
+hole(g, mat, bottom, d=POWER_D)
+hole(g, mat, POWER_OUTER_DEPTH, d=POWER_OUTER_D)
 
 # switch
 SWITCH_D = 4.0 # 3.6 # 3.5
-SWITCH_X = X_CAPSULE_NOM/2 + DZ_CENTER
-SWITCH_INSET_D = 5.0
-SWITCH_INSET_DEPTH = -1.0
+SWITCH_INSET_D = 7.0
+SWITCH_INSET_DEPTH = -2.0
 
-g.move(z=CNC_TRAVEL_Z)
-g.move(x=SWITCH_X, y=0)
-hole(g, mat, -H_WOOD - Z_PAD, SWITCH_D/2)
-
-if SMOOTH_PATHS:
-    # rounded inset 
-    hole(g, mat, SWITCH_INSET_DEPTH, SWITCH_INSET_D/2 - TOOL_R)
-else:
-    hole(g, mat, SWITCH_INSET_DEPTH, SWITCH_INSET_D/2)
-
-if SMOOTH_PATHS:
-    #### rounded tool ######
-    tool_change(g, 2)
-
-    g.move(z=CNC_TRAVEL_Z)
-    g.move(x=POWER_X, y=0)
-    # rounded inset
-    hole(g, mat, POWER_OUTER_DEPTH, POWER_OUTER_D/2)
-
-    g.move(z=CNC_TRAVEL_Z)
-    g.move(x=SWITCH_X, y=0)
-    hole(g, mat, -H_WOOD - Z_PAD, SWITCH_D/2)
-    # rounded inset 
-    hole(g, mat, SWITCH_INSET_DEPTH, SWITCH_INSET_D/2)
-
-    tool_change(g, 1)
+travel(g, mat, x=bounds.cx + DZ_CENTER)    
+hole(g, mat, bottom, d=SWITCH_D)
+hole(g, mat, SWITCH_INSET_DEPTH, d=SWITCH_INSET_D)
 
 #### capsule cut #######
-g.move(x=X_CAPSULE_NOM/2, y=0)
-g.move(z=0)
-
-capsule(g, mat, -H_WOOD - Z_PAD, 
-        X_CAPSULE_NOM, D_CAPSULE,
-        "outside", True, 'x')
+travel(g, mat, x=bounds.cx)
+capsule(g, mat, bottom, bounds.center.dx, bounds.center.dy, "outside", True, 'x')
