67dc7dd5ce84bd8a7f4b0ec365f1d05be51b03c8
Lee Thomason <leethomason@gmail.com>
2019-10-05 14:19:16 -0700

    fixes during cutting the part

--- parts/shock3/slots.py
+++ parts/shock3/slots.py
@@ -8,10 +8,8 @@ from rectangle import rectangle
 # cut the shock3
 # origin centered on switch, at first ridge
 
-CUT_DEPTH = -6
-
 # Basic
-DIAMETER = 18.0
+DIAMETER = 18.5
 INSET_L = 40.0
 INSET_CENTER_M = -63.1 / 2
 SPACE = 3.2
@@ -19,10 +17,12 @@ SPACE = 3.2
 CRYSTAL_0_SECTION_M = 13.15
 CRYSTAL_0_SECTION_L = 19.15
 CRYSTAL_0_L = CRYSTAL_0_SECTION_L - SPACE * 2
+CRYSTAL_0_M = CRYSTAL_0_SECTION_M + (CRYSTAL_0_SECTION_L - CRYSTAL_0_L) / 2
 
 CRYSTAL_1_SECTION_M = 34.6
 CRYSTAL_1_SECTION_L = 13.67
 CRYSTAL_1_L = CRYSTAL_1_SECTION_L - SPACE * 2
+CRYSTAL_1_M = CRYSTAL_1_SECTION_M + (CRYSTAL_1_SECTION_L - CRYSTAL_1_L) / 2
 
 OLED_M = 67.3 + 10.0
 OLED_L = 24.6
@@ -31,11 +31,10 @@ OLED_W = 8.3
 # GMatrix writes all of x, y, z, so moves must be fully specified.
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None, print_lines=False)
 mat = init_material(sys.argv[1])
+CUT_DEPTH = float(sys.argv[2])
 
 # Derived
 INSET_M = INSET_CENTER_M - INSET_L / 2
-CRYSTAL_0_M = CRYSTAL_0_SECTION_M + CRYSTAL_0_SECTION_L - CRYSTAL_0_SECTION_L
-CRYSTAL_1_M = CRYSTAL_1_SECTION_M + CRYSTAL_1_SECTION_L - CRYSTAL_1_SECTION_L
 R = DIAMETER / 2
 
 tool_size = mat['tool_size']
