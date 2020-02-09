71a3736a03835aaabf4749d5580058c905481534
Lee Thomason <leethomason@gmail.com>
2020-02-08 17:20:06 -0800

    field fixes.

--- parts/leia/dotstar.py
+++ parts/leia/dotstar.py
@@ -1,20 +1,25 @@
 from utility import *
 from material import init_material
 from hole import hole
+from drill import drill
 from mecode import G
 
 # inner line to inner line: 47mm
 # 2.5mm space
 # capsule at center
 
-mat = init_material("np883-aluminum-1.0")
+#mat = init_material("np883-aluminum-1.0")
+mat = init_material("np883-aluminum-3.275")
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
 nomad_header(g, mat, CNC_TRAVEL_Z)
 
 CY = 22.2 + 42.0 / 2.0
 PITCH = 7
-DEPTH = -6.0
+DEPTH = -1.0
 
+drill(g, mat, -4)
+
+'''
 g.spindle()
 travel(g, mat, y = CY - 1.5 * PITCH)
 g.dwell(1)
@@ -24,12 +29,15 @@ travel(g, mat, y = CY + 0.5 * PITCH)
 g.dwell(1)
 travel(g, mat, y = CY + 1.5 * PITCH)
 g.dwell(1)
+'''
 
-travel(g, mat, y = CY - 1.5 * PITCH)
+'''
+g.move(z=0)
+#hole(g, mat, DEPTH, d=1.6)
+travel(g, mat, y = 1 * PITCH)
 hole(g, mat, DEPTH, d=1.6)
-travel(g, mat, y = CY - 0.5 * PITCH)
+travel(g, mat, y = 2 * PITCH)
 hole(g, mat, DEPTH, d=1.6)
-travel(g, mat, y = CY + 0.5 * PITCH)
+travel(g, mat, y = 3 * PITCH)
 hole(g, mat, DEPTH, d=1.6)
-travel(g, mat, y = CY + 1.5 * PITCH)
-hole(g, mat, DEPTH, d=1.6)
+'''
--- parts/leia/switchplate.py
+++ parts/leia/switchplate.py
@@ -15,16 +15,14 @@ PORT = BOLT + 12.0
 SWITCH = BOLT - 12.0
 TRIM = 0.2
 
+bottom = -H_WOOD - 1.0
+
 mat = init_material(sys.argv[1])
 print("material:", mat)
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None, print_lines=False)
 nomad_header(g, mat, CNC_TRAVEL_Z)
 g.absolute()
 g.move(z=0)
-
-# cut the curve - expects a ball cutter
-hill(g, mat, D_OUTER, X_CAPSULE, Y_CAPSULE, False)
-tool_change(g, mat, 1)
 
 #center bolt
 travel(g, mat, x=BOLT)
@@ -43,10 +41,11 @@ hole(g, mat, bottom, d=BUTTON_HEAD_D + 0.2)
 D = 8.0
 SPACE = PORT - 11.0/2
 
-travel(g, mat, x=SWITCH - BUTTON_HEAD_D/2)
-rectangleTool(g, mat, -2.0, (X_CAPSULE - SPACE) - (SWITCH - BUTTON_HEAD_D/2), D, D/2, "left", "inner", True)
+D = 11.0
+travel(g, mat, x=-BUTTON_HEAD_D)
+rectangleTool(g, mat, -1.5, (SWITCH + BUTTON_HEAD_D/2 + 1.0) + BUTTON_HEAD_D, D, D/2, "left", "inner", True, adjust_trim=True)
 
 #### capsule cut #######
 travel(g, mat, x=TRIM/2)
-rectangleTool(g, mat, bottom, X_CAPSULE-TRIM, Y_CAPSULE-TRIM, 3.175/2, "left", "outer", False)
+rectangleTool(g, mat, bottom, X_CAPSULE-TRIM, Y_CAPSULE-TRIM, 3.175/2, "left", "outer", False, adjust_trim=True)
 
