556b6111884a0e28f46fa0c5645b17e42d4df38b
Lee Thomason <leethomason@gmail.com>
2021-02-28 17:11:03 -0800

    working on clan saber

---
+++ parts/clan/flipbutton.py
@@ -0,0 +1,44 @@
+import sys
+from utility import *
+from material import init_material
+from rectangleTool import rectangleTool
+from mecode import G
+from hole import hole
+
+mat = init_material(sys.argv[1])
+side = int(sys.argv[2])
+
+T_STOCK = 26.0
+H_HEAD = 4.5
+H_PIN = 6.5
+D_HEAD = 6.2
+D_PIN = 4.2
+H_INSERT = 2.0
+D_INSERT = 4.0
+
+toolSize = mat["tool_size"]
+
+g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
+nomad_header(g, mat, CNC_TRAVEL_Z)
+g.absolute()
+
+g.move(x=0, y=0)
+
+# cut pin from side 0
+if side == 0:
+    # cut down to pin
+    g.move(z=0)
+    hole(g, mat, -T_STOCK + H_HEAD + H_PIN, d=D_PIN + 0.5, fill=True)
+    # cut around pin
+    g.move(z=0)
+    hole(g, mat, -T_STOCK + H_HEAD, d=D_PIN + toolSize, offset="middle", fill=False)
+
+# cut head from side 1
+if side == 1:
+    # cut center hole
+    g.move(z=0)
+    hole(g, mat, -H_INSERT, d=D_INSERT)
+    # cut the head free
+    g.move(z=0)
+    hole(g, mat, -H_HEAD - 0.5, d=D_HEAD + toolSize, offset="middle", fill=False)
+
--- parts/clan/spacer.py
+++ parts/clan/spacer.py
@@ -7,13 +7,18 @@ from hole import hole
 
 mat = init_material(sys.argv[1])
 
-DZ_PLATE = 36.0
-W_PLATE = 19.0
-DZ_PORT, DZ_BOLT, DZ_SWITCH = 8, 19, 29.3
-H_PLATE = 7.575
+DZ_PLATE = 31.0
+W_PLATE = 15.0
+DZ_PORT, DZ_BOLT, DZ_SWITCH = 7.1, 17.5, 25.7
+
+T_HOLDER = 2.25
+T_STOCK = 6.45
+
 D_SWITCH = 8.2
 
-depth = -2.0
+depth = -T_STOCK - 0.5
+plane_depth = T_HOLDER - T_STOCK
+thick = T_STOCK > 2.0
 
 g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
 nomad_header(g, mat, CNC_TRAVEL_Z)
@@ -21,16 +26,22 @@ g.absolute()
 
 travel(g, mat, y=DZ_PORT)
 g.move(z=0)
-hole(g, mat, depth, d=11.0, fill=False)
+hole(g, mat, depth, d=11.0, fill=thick)
 
 travel(g, mat, y=DZ_BOLT)
 g.move(z=0)
-hole(g, mat, -2.0, d=8.0, fill=False)
+hole(g, mat, depth, d=8.0, fill=thick)
 
 travel(g, mat, y=DZ_SWITCH)
 g.move(z=0)
-hole(g, mat, depth, d=D_SWITCH, fill=False)
+hole(g, mat, depth, d=D_SWITCH, fill=thick)
 
+'''
+if plane_depth < 0:
+    travel(g, mat, x=0, y=0)
+    g.move(z=0)
+    rectangleTool(g, mat, plane_depth, W_PLATE, DZ_PLATE, 0, "bottom", "center", fill=True)
+'''
 travel(g, mat, x=0, y=0)
 g.move(z=0)
 rectangleTool(g, mat, depth, W_PLATE, DZ_PLATE, W_PLATE/2, "bottom", "outer", fill=False, adjust_trim=True)
