68caaaee2758311a6085df4d7e150e6c3d6e6abb
Lee Thomason <leethomason@gmail.com>
2021-03-13 15:24:05 -0800

    working on mark

--- parts/clan/makermark.py
+++ parts/clan/makermark.py
@@ -4,18 +4,37 @@ from material import init_material
 from rectangleTool import rectangleTool
 from mecode import G
 from hole import hole
+from drill import drill
 
-# makermark.py material depth1 depth2 odd_depth stride "center"/"bottom"
+# makermark.py material depth slope odd_depth stride "center"/"bottom"
 
 mat = init_material(sys.argv[1])
-depth1 = float(sys.argv[2])
-depth2 = float(sys.argv[3])
+depth = float(sys.argv[2])
+slope = float(sys.argv[3])
 odd_depth = float(sys.argv[4])
 stride = float(sys.argv[5])
 start = sys.argv[6]
 
 origin_y = 0
-if start == "bottom":
+if start == "center":
     origin_y = -stride * 1.5
 
+g = G(outfile='path.nc', aerotech_include=False, header=None, footer=None)
+nomad_header(g, mat, CNC_TRAVEL_Z)
+g.absolute()
 
+for i in range(0, 4):
+    g.feed(mat['feed_rate'])
+    g.move(z=CNC_TRAVEL_Z)
+    g.move(x=0, y=origin_y + stride * i)
+    g.move(z=1.0)
+
+    d = depth + slope * i / 3.0
+
+    if i == 2:
+        if odd_depth < 0:
+            drill(g, mat, odd_depth - 1.0)
+    else:
+        drill(g, mat, d - 1.0)
+
+g.move(z=10)
